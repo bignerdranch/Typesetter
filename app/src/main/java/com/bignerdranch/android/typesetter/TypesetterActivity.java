d8aae71dc0adf8e21514f631d75427b94d6bbe99
paul <pturner@bignerdranch.com>
2017-03-13 13:40:01 -0400

    Add empty Android project

---
+++ app/src/main/java/com/bignerdranch/android/typesetter/TypesetterActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.typesetter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TypesetterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_typesetter);
+    }
+}
